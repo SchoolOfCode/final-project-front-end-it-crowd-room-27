7110847896fddd9e0629cb6570515da3eb431f0d
Dmitriy Yegorov <yegorovd14@gmail.com>
2022-02-24 09:56:54 +0000

    work in progress - landing page

--- Components/Navbar/index.js
+++ Components/Navbar/index.js
@@ -4,25 +4,22 @@ import Link from "next/link";
 import Logo from "../../assets/logo-main.png";
 import Image from "next/image";
 
-// const StyledLink = styled.a`
-//   color: red;
-// `
 
 function Navbar() {
    return (
-      <div className={styles.container}>
-         <div className={styles.logo}>
+      <div className={styles.top_container}>
+	  	<div className={styles.logo}>
             <Link href="/">
                <a>
                   <Image src={Logo} height="120em" width="120em" />
                </a>
             </Link>
-         </div>
+        </div>
+	  
+	  	<div className={styles.container}>
+         
          <div className={styles.menu}>
             <ul>
-               <li className={styles.menuItem}>
-                  <Link href="/">Login</Link>
-               </li>
                <li className={styles.menuItem}>
                   <Link href="/about">About</Link>
                </li>
@@ -32,16 +29,22 @@ function Navbar() {
                <li className={styles.menuItem}>
                   <Link href="/blog">Blog</Link>
                </li>
+			    {/* {user ? <li className={styles.menuItem}>
+                  <Link href="/">Log Out</Link>
+               </li> : <li style={{display: "none"}}></li>} */}
             </ul>
          </div>
          <div className={styles.userImg}>
-            <Link href="/">
+            <Link href="/profile">
                <a>
+				   {/* {user ? <img src={user}/> : <img src="#" style={{display: "none"}}/>} */}
                   <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
                </a>
             </Link>
          </div>
       </div>
+
+	  </div>
    );
 }
 
