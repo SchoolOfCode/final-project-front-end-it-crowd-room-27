4747ea29ee55f98829a7beaf43ca802d00d5ecfc
thuannguyen88 <getaholdofthuan@gmail.com>
2022-02-24 15:44:59 +0000

    changed name of giveawaysubmitbutton to giveitembutton and created giveitembutton component

--- Components/GiveAwayModal/index.js
+++ Components/GiveAwayModal/index.js
@@ -1,24 +1,24 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
 import styles from "../../styles/giveAwayModal.module.css";
-import GiveAwaySubmitButton from "../Buttons/GiveAwaySubmitButton/index";
+import GiveItemSubmitButton from "../Buttons/GiveItemSubmitButton/index";
 function GiveAwayModal(props) {
-   return (
-      <Modal
-         {...props}
-         size="lg"
-         aria-labelledby="contained-modal-title-vcenter"
-         centered
-      >
-         <Modal.Header closeButton className={styles.header}>
-            <h1>Give Away Form</h1>
-         </Modal.Header>
-         <Modal.Body className={styles.body}></Modal.Body>
-         <Modal.Footer className={styles.footer}>
-            <GiveAwaySubmitButton />
-         </Modal.Footer>
-      </Modal>
-   );
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton className={styles.header}>
+        <h1>Give Away Form</h1>
+      </Modal.Header>
+      <Modal.Body className={styles.body}></Modal.Body>
+      <Modal.Footer className={styles.footer}>
+        <GiveItemSubmitButton />
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 export default GiveAwayModal;
