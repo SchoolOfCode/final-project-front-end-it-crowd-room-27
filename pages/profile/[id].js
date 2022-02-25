1be763da8c4c1b1c0381e4a86e99bd252d517edc
thuannguyen88 <getaholdofthuan@gmail.com>
2022-02-25 14:44:54 +0000

    successful dynamic routes for profiles

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -4,8 +4,8 @@ export const getStaticPaths = async () => {
   const res = await fetch("https://it-crowd-project.herokuapp.com/api/users");
   // const res = await fetch("https://jsonplaceholder.typicode.com/users/");
   const data = await res.json();
-  const user = data.payload;
-  const paths = user.map((profile) => {
+  const profiles = data.payload;
+  const paths = profiles.map((profile) => {
     return {
       params: { id: profile.id.toString() },
     };
@@ -25,10 +25,10 @@ export const getStaticProps = async (context) => {
 
   // const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
   const data = await res.json();
-  console.log(data);
+  console.log(data.payload);
   return {
     props: {
-      profile: data.payload,
+      profile: data.payload[0],
     },
   };
 };
@@ -39,8 +39,8 @@ const Details = ({ profile }) => {
       <h1>Details Page</h1>
       <h2>Irfan</h2>
       <p>{profile.first_name}</p>
-      {/* <p>{profile.email}</p> */}
-      {/* <p>{profile.website}</p> */}
+      {/* <p>{profile.email}</p>
+      <p>{profile.website}</p> */}
     </div>
   );
 };
