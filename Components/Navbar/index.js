f1216cf895e8604023acec220ab11e4b29346d81
Dmitriy Yegorov <42515766+AmidV@users.noreply.github.com>
2022-02-25 19:23:34 +0000

    Update index.js

--- Components/Navbar/index.js
+++ Components/Navbar/index.js
@@ -6,8 +6,15 @@ import Logo from "../../assets/logo-main.png";
 import Image from "next/image";
 import GiveAwayModal from "../GiveAwayModal";
 
-function Navbar() {
+function Navbar({ registeredUserAvatar, userEmail, users }) {
+   //Auth0
+   const { user, error, isLoading } = useUser();
+console.log(user)
+   if(isLoading) return <div>Loading ...</div>;
+    if(error) return <div>{error.message}</div>;
+  
   const [giveItemModalShow, setGiveItemModalShow] = React.useState(false);
+  
   return (
     <div className={styles.top_container}>
       <div className={styles.logo}>
@@ -44,22 +51,43 @@ function Navbar() {
             <li className={styles.menuItem}>
               <Link href="/blog">Blog</Link>
             </li>
-            {/* {user ? <li className={styles.menuItem}>
-                  <Link href="/">Log Out</Link>
-               </li> : <li style={{display: "none"}}></li>} */}
+            
+              <li className={styles.menuItem}>
+                  <a href="/api/auth/login">Log In</a>
+               </li>
+               {user && <li className={styles.menuItem}>
+                  <a href="/api/auth/logout">Log Out</a>
+               </li>}
+                
           </ul>
         </div>
         <div className={styles.userImg}>
-          <Link href="/profile">
-            <a>
-              {/* {user ? <img src={user}/> : <img src="#" style={{display: "none"}}/>} */}
-              <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-            </a>
-          </Link>
-        </div>
+            <Link href="/profile">
+               <a>
+				   { user ? 
+                  <img src={ registeredUserAvatar ? registeredUserAvatar : user.picture} 
+                       alt={user.first_name}/> : null }
+               </a>
+            </Link>
+         </div>
       </div>
     </div>
   );
 }
+
+export async function getServerSideProps() {
+    
+   const res = await fetch(`https://it-crowd-project.herokuapp.com/api/users`);
+   const data = await res.json();
+ 
+   // By returning { props: { allUsers } }, the PostAuth component
+   // will receive `allUsers` as a prop at BUILD time
+   return {
+       props: 
+           { users: data.payload },
+     }
+   
+}
+// "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
 
 export default Navbar;
