644236bd5a2f9cb14697f12c4f36cb544c284edc
irfanhussain <irfan185@gmail.com>
2022-02-25 14:15:16 +0000

    working but no render

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,11 +1,11 @@
 import React from "react";
 
 export const getStaticPaths = async () => {
-  // const res = await fetch("https://it-crowd-project.herokuapp.com/api/users");
-  const res = await fetch("https://jsonplaceholder.typicode.com/users/");
+  const res = await fetch("https://it-crowd-project.herokuapp.com/api/users");
+  // const res = await fetch("https://jsonplaceholder.typicode.com/users/");
   const data = await res.json();
-
-  const paths = data.map((profile) => {
+  const user = data.payload;
+  const paths = user.map((profile) => {
     return {
       params: { id: profile.id.toString() },
     };
@@ -19,15 +19,16 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async (context) => {
   const id = context.params.id;
-  // const res = await fetch(
-  //   `https://it-crowd-project.herokuapp.com/api/users/${id}`
-  // );
-  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const res = await fetch(
+    "https://it-crowd-project.herokuapp.com/api/users/" + id
+  );
+
+  // const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
   const data = await res.json();
   console.log(data);
   return {
     props: {
-      profile: data,
+      profile: data.payload,
     },
   };
 };
@@ -37,9 +38,9 @@ const Details = ({ profile }) => {
     <div>
       <h1>Details Page</h1>
       <h2>Irfan</h2>
-      <p>{profile.name}</p>
-      <p>{profile.email}</p>
-      <p>{profile.website}</p>
+      <p>{profile.first_name}</p>
+      {/* <p>{profile.email}</p> */}
+      {/* <p>{profile.website}</p> */}
     </div>
   );
 };
