ccab020bb3dba570a633048786ebcfa3fe341019
thuannguyen88 <getaholdofthuan@gmail.com>
2022-03-02 12:35:19 +0000

    ...

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,49 +1,49 @@
-import React from "react";
+// import React from "react";
 
-export const getServerSidePaths = async () => {
-  const res = await fetch("https://it-crowd-project.herokuapp.com/api/users");
-  // const res = await fetch("https://jsonplaceholder.typicode.com/users/");
-  const data = await res.json();
-  const profiles = data.payload;
-  const paths = profiles.map((profile) => {
-    return {
-      params: { id: profile.id.toString() },
-    };
-  });
+// export const getServerSidePaths = async () => {
+//   const res = await fetch("https://it-crowd-project.herokuapp.com/api/users");
+//   // const res = await fetch("https://jsonplaceholder.typicode.com/users/");
+//   const data = await res.json();
+//   const profiles = data.payload;
+//   const paths = profiles.map((profile) => {
+//     return {
+//       params: { id: profile.id.toString() },
+//     };
+//   });
 
-  return {
-    paths,
-    fallback: false,
-  };
-};
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
-export const getServerSideProps = async (context) => {
-  const id = context.params.id;
-  const res = await fetch(
-    "https://it-crowd-project.herokuapp.com/api/users/" + id
-  );
+// export const getServerSideProps = async (context) => {
+//   const id = context.params.id;
+//   const res = await fetch(
+//     "https://it-crowd-project.herokuapp.com/api/users/" + id
+//   );
 
-  // const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
-  const data = await res.json();
-  console.log(data.payload);
-  return {
-    props: {
-      profile: data.payload[0],
-    },
-  };
-};
+//   // const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+//   const data = await res.json();
+//   console.log(data.payload);
+//   return {
+//     props: {
+//       profile: data.payload[0],
+//     },
+//   };
+// };
 
-const Details = ({ profile }) => {
-  return (
-    <div>
-      <h1>Details Page</h1>
-      <h2>Irfan</h2>
-      <p>{profile.first_name}</p>
-      {/* <p>{profile.email}</p>
-      <p>{profile.website}</p> */}
-    </div>
-  );
-};
-export const getServerSideProps = withPageAuthRequired();
+// const Details = ({ profile }) => {
+//   return (
+//     <div>
+//       <h1>Details Page</h1>
+//       <h2>Irfan</h2>
+//       <p>{profile.first_name}</p>
+//       {/* <p>{profile.email}</p>
+//       <p>{profile.website}</p> */}
+//     </div>
+//   );
+// };
+// export const getServerSideProps = withPageAuthRequired();
 
-export default Details;
+// export default Details;
