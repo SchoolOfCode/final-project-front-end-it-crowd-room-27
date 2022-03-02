d765a35dc02e89a49875f49869d452d1e4411704
irfanhussain <irfan185@gmail.com>
2022-03-02 17:02:08 +0000

    styling complete

--- Components/LandingPageComps/landingPageComps.js
+++ Components/LandingPageComps/landingPageComps.js
@@ -2,13 +2,13 @@ import React from "react";
 import styles from "../../styles/home.module.css";
 import ReadMoreButton from "../Buttons/ReadMoreButton/Index";
 
-export function Top() {
+export function Banner() {
   return (
     <div className={styles.cover}>
       <div className={styles.top}>
         <div className={styles.inner_top}>
           <h1 className={styles.title}>
-            iGive <br /> with pleasure!
+            Ready to fight<br></br> food waste?
           </h1>
         </div>
       </div>
@@ -16,102 +16,125 @@ export function Top() {
   );
 }
 
-export function Middle() {
+export function SectionOne() {
   return (
-    <div className={styles.midle}>
-      <h3 className={styles.arttitle}>Our Story!</h3>
-      <p className={styles.article}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime
-        possimus quidem fugiat illum sint! Inventore cumque quod sequi, quas
-        provident, voluptas aspernatur deleniti officiis deserunt porro dolores
-        excepturi soluta voluptatibus, odit esse aliquam et harum nihil dolor
-        maxime dolorum praesentium. Nobis quis nam ea nihil facilis atque
-        deserunt libero magnam totam asperiores corporis tempore at quidem,
-        provident, beatae veniam veritatis. Obcaecati cupiditate nam
-        repellendus, autem provident, harum eligendi expedita ipsam nulla
-        dolores aliquam mollitia tenetur ex ut? Quisquam, facere ducimus rem
-        aliquam voluptas quas necessitatibus a. Repellendus architecto quasi
-        omnis possimus eos consequuntur, vitae sapiente? Nihil provident dolorem
-        neque reiciendis?
+    <div className={styles.flexboxContainer}>
+      <div className={`${styles.flexItem1}`}>
+        <h3 className={styles.arttitle}>Our Story!</h3>
+        <p className={styles.article}>
+          Food waste is one of the largest contributors to climate change. At
+          the moment many products are made, used and needlessly thrown away,
+          contributing to climate change and environmental damage.<br></br>
+          <br></br> Many people would prefer a lower carbon and lower cost
+          solution for their food which we can enable through our app ‘iGive’.
+          <br></br>
+          <br></br>iGive is a community based app for people to share their
+          unwanted products which could be useful elsewhere. Unwanted food items
+          can be shared through the community by using our app.
+        </p>
+        {/* <ReadMoreButton /> */}
+      </div>
+    </div>
+  );
+}
+
+export function SectionTwo() {
+  return (
+    <div className={styles.flexboxContainer}>
+      <div className={styles.flexItem1}>
+        <h3 className={styles.useDescription}>How iGive Works!</h3>
+        <iframe
+          className={styles.video}
+          src="https://www.youtube.com/embed/dnKFSafaJOo"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        {/* width="480" height="315" */}
+      </div>
+    </div>
+  );
+}
+
+export function SectionThree() {
+  return (
+    <div className={styles.flexboxContainer}>
+      <div className={styles.flexItem1}>
+        <h3 className={styles.cards_title}>
+          Hear What Our<br></br> Customers are Saying
+        </h3>
+
+        <div className={styles.cards_cover}>
+          <div className={styles.card}>
+            <p className={styles.comment_author}>
+              "I used to throw away veggies that were about to expire. Now I
+              save money and reduce food waste"
+            </p>
+            <span className={styles.raiting_author}>5 stars</span>
+            <p className={styles.feedback_author}>Mike From Derby</p>
+          </div>
+
+          <div className={styles.card}>
+            <p className={styles.comment_author}>
+              "I used to throw away veggies that were about to expire. Now I
+              save money and reduce food waste"
+            </p>
+            <span className={styles.raiting_author}>5 stars</span>
+            <p className={styles.feedback_author}>Mike From Derby</p>
+          </div>
+
+          <div className={styles.card}>
+            <p className={styles.comment_author}>
+              "I used to throw away veggies that were about to expire. Now I
+              save money and reduce food waste"
+            </p>
+            <span className={styles.raiting_author}>5 stars</span>
+            <p className={styles.feedback_author}>Mike From Derby</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function SectionFour() {
+  return (
+    <div className={styles.flexboxContainer}>
+      <div className={styles.flexItem1}>
+        <h3 className={styles.cards_title}>Featured In</h3>
+        <div className={styles.flexItem2}>
+          <img
+            className={styles.featured_logo}
+            src="https://www.chalegrove.co.uk/wp-content/uploads/2018/12/sunday-times.jpg"
+          />
+          <img
+            className={styles.featured_logo}
+            src="https://www.logolynx.com/images/logolynx/2d/2d71f43a8bca9f5c33ad507415e303af.jpeg"
+          />
+          <img
+            className={styles.featured_logo}
+            src="https://media.info/i/lf/0/1442315133/1571.png"
+          />
+          <img
+            className={styles.featured_logo}
+            src="https://www.tabletalkmedia.co.uk/wp-content/uploads/2015/04/hello.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+export function Copyright() {
+  return (
+    <div className={styles.footer}>
+      <a href="#" className={styles.btn_up}>
+        Pop Up
+      </a>
+      <p className={styles.cast}>
+        created by <strong>IT-Crowd</strong>
+        <br /> Simren, Lilly-Ane, Irfan, Thuan, Rory, Dmitriy
       </p>
-      <ReadMoreButton />
-    </div>
-  );
-}
-
-export function Bottom() {
-  return (
-    <div className={styles.bottom}>
-      <h3 className={styles.useDescription}>How to use iGive</h3>
-      <iframe
-        className={styles.video}
-        src="https://www.youtube.com/embed/dnKFSafaJOo"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-      {/* width="480" height="315" */}
-    </div>
-  );
-}
-
-export function VeryBottom() {
-  return (
-    <div className={styles.very_bottom}>
-      <h3 className={styles.cards_title}>
-        Hear What Our<br></br> Customers are Saying
-      </h3>
-
-      <div className={styles.cards_cover}>
-        <div className={styles.card}>
-          <p className={styles.comment_author}>
-            "I used to throw away veggies that were about to expire. Now I save
-            money and reduce food waste"
-          </p>
-          <span className={styles.raiting_author}>5 stars</span>
-          <p className={styles.feedback_author}>Mike From Derby</p>
-        </div>
-
-        <div className={styles.card}>
-          <p className={styles.comment_author}>
-            "I used to throw away veggies that were about to expire. Now I save
-            money and reduce food waste"
-          </p>
-          <span className={styles.raiting_author}>5 stars</span>
-          <p className={styles.feedback_author}>Mike From Derby</p>
-        </div>
-
-        <div className={styles.card}>
-          <p className={styles.comment_author}>
-            "I used to throw away veggies that were about to expire. Now I save
-            money and reduce food waste"
-          </p>
-          <span className={styles.raiting_author}>5 stars</span>
-          <p className={styles.feedback_author}>Mike From Derby</p>
-        </div>
-      </div>
-
-      <h3 className={styles.cards_title}>Featured In</h3>
-      <div className={styles.featured_media}>
-        <img
-          className={styles.featured_logo}
-          src="https://www.chalegrove.co.uk/wp-content/uploads/2018/12/sunday-times.jpg"
-        />
-        <img
-          className={styles.featured_logo}
-          src="https://www.logolynx.com/images/logolynx/2d/2d71f43a8bca9f5c33ad507415e303af.jpeg"
-        />
-        <img
-          className={styles.featured_logo}
-          src="https://media.info/i/lf/0/1442315133/1571.png"
-        />
-        <img
-          className={styles.featured_logo}
-          src="https://www.tabletalkmedia.co.uk/wp-content/uploads/2015/04/hello.png"
-        />
-      </div>
-      {/* </div> */}
     </div>
   );
 }
