5eee43221295d433b2dbb687da24bc1d796b9ada
thuannguyen88 <getaholdofthuan@gmail.com>
2022-03-07 15:24:16 +0000

    created component for scroll button to be used elsewhere if needed

---
+++ Components/ScrollToTop/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "../../styles/home.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+
+function ScrollArrow() {
+  return (
+    <div className={styles.iconContainer}>
+      <a href="#" className={styles.btn_up}>
+        <FontAwesomeIcon
+          icon={faChevronUp}
+          size={"3x"}
+          className={styles.faIcon}
+        />
+      </a>
+    </div>
+  );
+}
+
+export default ScrollArrow;
