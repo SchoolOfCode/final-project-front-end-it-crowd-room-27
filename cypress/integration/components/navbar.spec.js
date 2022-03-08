388b82783f827ab3ea0add31ed792e6480587095
Simren Chandan <simrenkaursoor@gmail.com>
2022-03-08 22:29:00 +0000

    added test for navbar link

--- cypress/integration/components/navbar.spec.js
+++ cypress/integration/components/navbar.spec.js
@@ -16,15 +16,24 @@ describe("user visits landing page", () => {
          cy.get("#__next").find("li").first().should("have.text", "Home");
       });
 
-      it("check all links on navbar", () => {
-         cy.visit("/");
-         cy.get("li").each((page) => {
-            cy.request(page.prop("href"));
-         });
+      it("should render the About page once clicking on About on navbar", () => {
+         cy.get("#navbar_menuItem__EyQsE")
+            .find("li")
+            .next()
+            .should("have.text", "About");
       });
-      // it("should render the About page once clicking on About on navbar", () => {
-      //    cy.get("#__next").find("li").should("have.text", "About");
+
+      //  it("should find a link with an href attribute containing About", () => {
+      //    cy.get('a[href="About"]').click();
       // });
+      // "navbar_menu__GrkEK"
+      // it("check all links on navbar", () => {
+      //    cy.visit("/");
+      //    cy.get("li").each((page) => {
+      //       cy.request(page.prop("href"));
+      //    });
+      // });
+      //
 
       // it("should render the blog once clicking on navbar", () => {
       //    cy.get("#__next").find("li").should("have.text", "Blog");
