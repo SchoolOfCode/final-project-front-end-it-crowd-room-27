19b2c6fe163988f710144038832bddfb0ce758ed
thuannguyen88 <getaholdofthuan@gmail.com>
2022-03-09 10:26:06 +0000

    lifted up toast logic to the app level and passed props to listings and profile page

--- pages/_app.js
+++ pages/_app.js
@@ -4,26 +4,98 @@ import { UserProvider } from "@auth0/nextjs-auth0";
 import Router from "next/router";
 import { useState } from "react";
 import Loader from "../Components/Loader";
+import styles from "../styles/alert.module.css";
 
 function MyApp({ Component, pageProps }) {
-	const [loading, setLoading] = useState(false);
-	Router.events.on("routeChangeStart", (url) => {
-		console.log("loading");
-		setLoading(true);
-	});
-	Router.events.on("routeChangeComplete", (url) => {
-		console.log("loaded");
-		setLoading(false);
-	});
+  const [loading, setLoading] = useState(false);
+  const [list, setList] = useState([]);
+  const [isShowAlert, setIsShowAlert] = useState(false);
 
-	return (
-		<>
-			{loading && <Loader />}
-			<UserProvider>
-				<Component {...pageProps} />
-			</UserProvider>
-		</>
-	);
+  Router.events.on("routeChangeStart", (url) => {
+    console.log("loading");
+    setLoading(true);
+  });
+  Router.events.on("routeChangeComplete", (url) => {
+    console.log("loaded");
+    setLoading(false);
+  });
+
+  //   console.log(list);
+  let toastProperties = null;
+
+  const showToast = (type) => {
+    // console.log("show toast");
+    switch (type) {
+      case "green":
+        toastProperties = {
+          id: 1,
+          title: "success",
+          description: "this is a success message",
+          backgroundColor: "#5cb85c",
+        };
+        break;
+      case "red":
+        toastProperties = {
+          id: 2,
+          title: "danger",
+          description: "this is a danger message",
+          backgroundColor: "#d9534f",
+        };
+        break;
+      case "blue":
+        toastProperties = {
+          id: 3,
+          title: "info",
+          description: "this is an info message",
+          backgroundColor: "#5bc0de",
+        };
+        break;
+      case "orange":
+        toastProperties = {
+          id: 4,
+          title: "warning",
+          description: "this is a warning message",
+          backgroundColor: "#f0ad4e",
+        };
+        break;
+      case "item_upload_success":
+        toastProperties = {
+          id: 5,
+          title: "success",
+          description: "Amazing! Your item has been successfully uploaded",
+          backgroundColor: "#5cb85c",
+        };
+        break;
+      case "profile_update_success":
+        toastProperties = {
+          id: 6,
+          title: "success",
+          description: "Amazing! Your profile updated successfully",
+          backgroundColor: "#5cb85c",
+        };
+        break;
+      default:
+        toastProperties = [];
+    }
+
+    // setList([...list, toastProperties]);
+    setList([toastProperties]);
+  };
+
+  return (
+    <>
+      {loading && <Loader />}
+      <UserProvider>
+        <Component
+          {...pageProps}
+          toastList={list}
+          isShowAlert={isShowAlert}
+          setIsShowAlert={setIsShowAlert}
+          showToast={showToast}
+        />
+      </UserProvider>
+    </>
+  );
 }
 
 export default MyApp;
--- pages/listings.js
+++ pages/listings.js
@@ -8,63 +8,78 @@ import styles from "../styles/alert.module.css";
 
 console.log(API_URL);
 
-export default function Listings({ users, listings }) {
-  const [list, setList] = useState([]);
-  const [isShowAlert, setIsShowAlert] = useState(false);
+export default function Listings({
+  users,
+  listings,
+  toastList,
+  isShowAlert,
+  setIsShowAlert,
+  showToast,
+}) {
+  // const [list, setList] = useState([]);
+  // const [isShowAlert, setIsShowAlert] = useState(false);
 
-  console.log(list);
-  let toastProperties = null;
+  // console.log(list);
+  // let toastProperties = null;
 
-  const showToast = (type) => {
-    // console.log("show toast");
-    switch (type) {
-      case "green":
-        toastProperties = {
-          id: 1,
-          title: "success",
-          description: "this is a success message",
-          backgroundColor: "#5cb85c",
-        };
-        break;
-      case "red":
-        toastProperties = {
-          id: 2,
-          title: "danger",
-          description: "this is a danger message",
-          backgroundColor: "#d9534f",
-        };
-        break;
-      case "blue":
-        toastProperties = {
-          id: 3,
-          title: "info",
-          description: "this is an info message",
-          backgroundColor: "#5bc0de",
-        };
-        break;
-      case "orange":
-        toastProperties = {
-          id: 4,
-          title: "warning",
-          description: "this is a warning message",
-          backgroundColor: "#f0ad4e",
-        };
-        break;
-      case "item_upload_success":
-        toastProperties = {
-          id: 5,
-          title: "success",
-          description: "Amazing! Your item has been successfully uploaded",
-          backgroundColor: "#5cb85c",
-        };
-        break;
-      default:
-        toastProperties = [];
-    }
+  // const showToast = (type) => {
+  //   // console.log("show toast");
+  //   switch (type) {
+  //     case "green":
+  //       toastProperties = {
+  //         id: 1,
+  //         title: "success",
+  //         description: "this is a success message",
+  //         backgroundColor: "#5cb85c",
+  //       };
+  //       break;
+  //     case "red":
+  //       toastProperties = {
+  //         id: 2,
+  //         title: "danger",
+  //         description: "this is a danger message",
+  //         backgroundColor: "#d9534f",
+  //       };
+  //       break;
+  //     case "blue":
+  //       toastProperties = {
+  //         id: 3,
+  //         title: "info",
+  //         description: "this is an info message",
+  //         backgroundColor: "#5bc0de",
+  //       };
+  //       break;
+  //     case "orange":
+  //       toastProperties = {
+  //         id: 4,
+  //         title: "warning",
+  //         description: "this is a warning message",
+  //         backgroundColor: "#f0ad4e",
+  //       };
+  //       break;
+  //     case "item_upload_success":
+  //       toastProperties = {
+  //         id: 5,
+  //         title: "success",
+  //         description: "Amazing! Your item has been successfully uploaded",
+  //         backgroundColor: "#5cb85c",
+  //       };
+  //       break;
+  //     case "profile_update_success":
+  //       toastProperties = {
+  //         id: 6,
+  //         title: "success",
+  //         description: "Amazing! Your profile updated successfully",
+  //         backgroundColor: "#5cb85c",
+  //       };
+  //       break;
+  //     default:
+  //       toastProperties = [];
+  //   }
 
-    // setList([...list, toastProperties]);
-    setList([toastProperties]);
-  };
+  //   // setList([...list, toastProperties]);
+  //   setList([toastProperties]);
+  // };
 
   return (
     <>
@@ -78,7 +93,7 @@ export default function Listings({ users, listings }) {
         />
 
         <Toast
-          toastList={list}
+          toastList={toastList}
           position="top-right"
           isShowAlert={isShowAlert}
           setIsShowAlert={setIsShowAlert}
