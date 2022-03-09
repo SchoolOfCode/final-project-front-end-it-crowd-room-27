1752a7d8d46662edb10c1b3f9e9a9312189821ef
Simren Chandan <simrenkaursoor@gmail.com>
2022-03-09 00:09:19 +0000

    added test for give item button on navbar

--- cypress/integration/components/landingpage.spec.js
+++ cypress/integration/components/landingpage.spec.js
@@ -4,8 +4,27 @@ describe("when user visits landing page", () => {
          cy.visit("http://localhost:3000");
       });
 
-      it("should render the landing page and display a message", () => {
-         cy.get("cy.get('body')").contains("iGive with pleasure");
+      it("should render the landing page and display a message in the heading", () => {
+         cy.get("body").contains("food waste");
+      });
+      it("user should click on login button on navbar", () => {
+         cy.get("#__next").find("li").last().should("have.text", "Log In");
+      });
+
+      it("use requests to navigation bar links", () => {
+         const pages = ["Listings", "About"];
+
+         cy.visit("/");
+
+         pages.forEach((page) => {
+            cy.contains(page).then((link) => {
+               cy.request(link.prop("href"));
+            });
+         });
+      });
+
+      it("give item button should open up give away modal from navbar", () => {
+         cy.get(".navbar_btn__aeUzp").should("be.visible").click();
       });
    });
 });
